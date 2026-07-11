317a1924f650c32c80088b2e5275b396168fb12c
aaishadhungana <aaishadhu@gmail.com>
2026-07-11 11:07:02 -0400

    versions

---
+++ alembic/versions/2026_06_30_0301-e4c64c56e3cf_create_users_table.py
@@ -0,0 +1,34 @@
+from typing import Sequence, Union
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = 'e4c64c56e3cf'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table('users',
+    sa.Column('email', sa.String(length=255), nullable=False),
+    sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    sa.Column('full_name', sa.String(length=150), nullable=False),
+    sa.Column('phone', sa.String(length=20), nullable=True),
+    sa.Column('role', sa.Enum('parent', 'principal', 'operations', 'admin', name='user_role'), nullable=False),
+    sa.Column('id', sa.UUID(), server_default=sa.text('gen_random_uuid()'), nullable=False),
+    sa.Column('created_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.Column('updated_at', sa.DateTime(timezone=True), server_default=sa.text('now()'), nullable=False),
+    sa.Column('is_active', sa.Boolean(), server_default='true', nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_users_email'), 'users', ['email'], unique=True)
+    op.create_index(op.f('ix_users_role'), 'users', ['role'], unique=False)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_users_role'), table_name='users')
+    op.drop_index(op.f('ix_users_email'), table_name='users')
+    op.drop_table('users')
+   
+    op.execute("DROP TYPE IF EXISTS user_role")
